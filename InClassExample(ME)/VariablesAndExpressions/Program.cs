6e7662f087a9c12a6106a137b34d9d7ab469ea43
ghern <ghern@DESKTOP-OCUVVH0>
2021-09-02 10:30:59 -0500

    InClassExample(1)

---
+++ InClassExample(ME)/VariablesAndExpressions/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace VariablesAndExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Instructor";
+            string lastName = "Ackerman";
+            Console.WriteLine("Boomer Sooner");
+
+            Console.WriteLine("What is your first name? >>");
+            firstName = Console.ReadLine();
+
+            Console.WriteLine("What is your last name? >>");
+            lastName = Console.ReadLine();
+
+            int luckynumber = 7;
+            double evenluckierNumber = 7 * luckynumber;
+
+            Console.WriteLine(firstName + " " + lastName + " " + evenluckierNumber);
+        }
+    }
+}
