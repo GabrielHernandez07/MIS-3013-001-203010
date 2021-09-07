a58e77074a8afd6b979033f151cfe058f804d7f1
ghern <ghern@DESKTOP-OCUVVH0>
2021-09-07 11:42:25 -0500

    InClass

---
+++ FlowControl_COnditional_Ex.1/FlowControl_COnditional_Ex.1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace FlowControl_COnditional_Ex._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to my wonderful application! What is your name? >>");
+            string name = Console.ReadLine();
+
+            Console.WriteLine($"{name}, what is your monthly income? >>");
+            string monthlyIncomeAsString = Console.ReadLine();
+            double monthlyIncome = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"{name}, what is your monthly expenses? >>");
+            double monthlyExpenses = Convert.ToDouble(Console.ReadLine());
+
+            double monthlyNetAmount = monthlyIncome - monthlyExpenses;
+
+            if (monthlyNetAmount >= 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Beep(5000, 500);
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Beep(2000, 500);
+            }
+
+            Console.WriteLine(monthlyNetAmount.ToString("C2"));
+        }
+    }
+}
